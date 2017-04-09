f52644bcf18aef4e1e9c442d343d21caf422fb1a
IamEvaShih <eva.shih@iss.nthu.edu.tw>
2017-04-09 15:13:35 +0800

    Update Project 1_EE.py

--- Project 1_EE.py
+++ Project 1_EE.py
@@ -1,43 +1,22 @@
 
 # coding: utf-8
 
-# In[1]:
-
 import json
-
-
-# In[2]:
 
 theses = json.load(open('nthu_thesis20170330.json'))
 
-
-# In[3]:
-
 theses[0].index('系所名稱')
-
-
-# In[4]:
 
 theses[0].index('中文關鍵詞')
 
-
-# In[5]:
 
 ##把系所 與 英文關鍵字 個別抓出
 data_list = [(thesis[2], thesis[17].split()) for thesis in theses[1:]]
 
 
-# In[6]:
-
 data_list
 
-
-# In[7]:
-
 index = []
-
-
-# In[8]:
 
 #先列出所有的系所
 i=0
@@ -45,10 +24,8 @@ while i < 5444:
     if data_list[i][0] not in index:
         index.append(data_list[i][0])
     i+=1
+    
 index
-
-
-# In[9]:
 
 ##這裡以電機工程學系為例，其他45個系所也是同樣照此步驟
 ##把list中 屬於電機工程學系的關鍵字，都加入ee這個list中
@@ -59,13 +36,8 @@ while j < len(data_list):
         ee.append(data_list[j][1])
     j+=1
 
-
-# In[10]:
-
 ee
 
-
-# In[11]:
 
 ##去除list中的list 完全轉換為一個單一list
 flat_ee = []
@@ -75,22 +47,16 @@ for sublist in ee:
 flat_ee
 
 
-# In[12]:
-
 #開始計算字的數量
 from collections import Counter
 counter_ee = Counter(flat_ee)
 counter_ee
 
 
-# In[13]:
-
 #把它從次數高到低
 sort_ee = sorted(counter_ee.items(), key=lambda x: -x[1])
 sort_ee
 
-
-# In[14]:
 
 ##要把它轉換為 wordcloud網站的格式 前面次數 後面是字
 m=0
@@ -98,9 +64,7 @@ while m < len(sort_ee):
     print (sort_ee[m][1], sort_ee[m][0])
     m=m+1
 
-
-# In[15]:
-
+    
 ##再把上面結果複製到：
 ## https://timdream.org/wordcloud2.js/#les-miz  產生word cloud
 ##各系所的word cloud共46個，存放在一同繳交的pdf當中，包含priject的細項說明。
